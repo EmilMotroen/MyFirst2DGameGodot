88e6c5ad77f64a458c7a0804392dfbc5946bb3a3
Emil Motrøen <e.motroen@gmail.com>
2024-04-28 00:58:21 +0200

    Add basic Mob

--- player.gd
+++ player.gd
@@ -5,12 +5,14 @@ signal hit
 var screen_size
 
 # Called when the node enters the scene tree for the first time.
+# Same as Start() in Unity
 func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+# Same as Update() in Unity
 func _process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_up"):
@@ -38,3 +40,15 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _on_body_entered(body):
+	hide() # Player disappears after being hit
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
